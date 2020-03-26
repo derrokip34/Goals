9a48b4ac28bee83b0ba32cb4a789132ae3521f5b
derrickip34 <derrickip34@gmail.com>
2020-03-26 13:03:34 +0300

    Allow user to delete goals

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -19,9 +19,19 @@ export class GoalComponent implements OnInit {
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
+
   completeGoal(isComplete, index){
     if (isComplete) {
       this.goals.splice(index,1);
+    }
+  }
+  deleteGoal(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
     }
   }
 
